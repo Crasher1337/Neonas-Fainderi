e673c74f77eb335a89fabe152504a798a1c81f60
Crasher1337 <50756299+Crasher1337@users.noreply.github.com>
2022-03-31 22:39:40 +0400

    Update changecolor.js

--- changecolor.js
+++ changecolor.js
@@ -34,33 +34,33 @@ function myFunction7() {
 
 function background() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/1.png')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/1.png')";
 }
 function background1() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/2.jpg')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/2.jpg')";
 }
 function background2() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/3.png')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/3.png')";
 }
 function background3() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/4.png')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/4.png')";
 }
 function background4() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/5.jpg')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/5.jpg')";
 }
 function background5() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/6.jpg')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/6.jpg')";
 }
 function background6() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/7.jpg')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/7.jpg')";
 }
 function background7() {
   document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('img/8.png')";
+  document.body.style.backgroundImage = "url('https://crasher1337.github.io/Neonas-Fainderi/img/8.png')";
 }
