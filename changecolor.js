2ccf7bb49815c86c446e5b6230d7ad8868f2d53e
Crasher1337 <50756299+Crasher1337@users.noreply.github.com>
2022-03-31 22:11:44 +0400

    Add files via upload

---
+++ changecolor.js
@@ -0,0 +1,66 @@
+function myFunction() {
+  document.body.style.backgroundColor = "#000";
+}
+
+function myFunction1() {
+  document.body.style.backgroundColor = "#9c9c9c";
+}
+
+function myFunction2() {
+  document.body.style.backgroundColor = "#ff0000";
+}
+
+function myFunction3() {
+  document.body.style.backgroundColor = "#fbff00";
+}
+
+function myFunction4() {
+  document.body.style.backgroundColor = "#00ff11";
+}
+
+function myFunction5() {
+  document.body.style.backgroundColor = "#1e00ff";
+}
+
+
+function myFunction6() {
+  document.body.style.backgroundColor = "#292352";
+}
+
+
+function myFunction7() {
+  document.body.style.backgroundColor = "#292352";
+}
+
+function background() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/1.png')";
+}
+function background1() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/2.jpg')";
+}
+function background2() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/3.png')";
+}
+function background3() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/4.png')";
+}
+function background4() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/5.jpg')";
+}
+function background5() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/6.jpg')";
+}
+function background6() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/7.jpg')";
+}
+function background7() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('img/8.png')";
+}
